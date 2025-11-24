6c0d1e54a509bba64c6f878229ac189a240a78c4
DipakSirsath312 <dipaksirsath312@gmail.com>
2025-11-24 23:38:05 +0530

    [MODIFIED]:Dipak - Create the Error response and add cleanMarkdown also create addMessage for user&chatbot

--- App.js
+++ App.js
@@ -24,4 +24,29 @@ async function generateResponse(prompt) {
             ]
         })
     })
+
+    if(!response.ok){
+        throw new Error('Failed to give response');
+    }
+
+    const data = await response.json()
+    return data.candidates[0].content.parts[0].text;
+}
+
+function cleanMarkdown(text){
+    return text
+    .replace(/#{1,6}\s?/g,'')
+
+    .replace(/\*\*/g,'')
+
+    .replace(/\n{3,}/g,'\n\n')
+
+    .trim()
+}
+
+function addMessage(message,isUser){
+    const messageElement = document.createElement('div');
+    messageElement.classList.add('message')
+
+    messageElement.classList.add(isUser? 'user-message' : 'bot-message')
 }
